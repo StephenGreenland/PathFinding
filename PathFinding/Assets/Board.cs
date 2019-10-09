a29e046742e1e5fae6fec1c73e21f4c8f5cc5e1e
Stephen <1013573@student.sae.edu.au>
2019-10-09 17:16:57 +1100

    Some errors

--- PathFinding/Assets/Board.cs
+++ PathFinding/Assets/Board.cs
@@ -8,7 +8,7 @@ public class Board : MonoBehaviour
     public GameObject wall;
     private bool hasStart;
     private bool hasEnd;
-    public GameObject pathBoi;
+    public Vector2 pathBoi;
     public int height;
     public int width;
     public GameObject start;
@@ -16,12 +16,15 @@ public class Board : MonoBehaviour
     private int ballout;
     private Vector2 startPos;
     private Vector2 endPos;
+    private bool hasPos;
+    private Vector2 lastSpot;
     
     
     // Start is called before the first frame update
     void Start()
     {
         
+        hasPos = false;
         hasEnd = false;
         hasStart = false;    
         grid = new int[height,width];
@@ -42,6 +45,8 @@ public class Board : MonoBehaviour
             startPos = FindRandomEmptyLocation();
             hasStart = true;
             Instantiate(start, new Vector3(startPos.x, 0, startPos.y), Quaternion.identity);
+            lastSpot = startPos;
+
         }
         ballout = 0;
         while (!hasEnd)
@@ -59,11 +64,9 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Space"))
-        {
+
             FindPath();
-        }
-        
+            
         
     }
 
@@ -91,8 +94,80 @@ public class Board : MonoBehaviour
 
      private void FindPath()
      {
-         
+         if (!hasPos)
+         {
+             pathBoi = startPos;
+             hasPos = true;
+         }
+         if (pathBoi != endPos)
+         {
+             pathBoi = CheckSpaces();
+         }
+         else
+         {
+             print("you win");
+         }
          
      }
+
+      private Vector2 CheckSpaces()
+      {
+          Vector2 whereToMove;
+          List<Vector2> isVaild;
+          isVaild = new List<Vector2>();
+          
+          // check 4 directions
+          if (pathBoi.x != width)
+          {
+              if (grid[(int) pathBoi.x + 1, (int) pathBoi.y] != 1)
+              {
+                  isVaild.Add(new Vector2(pathBoi.x + 1, pathBoi.y));
+              }
+          }
+
+          if (pathBoi.y != height)
+          {
+              if (grid[(int) pathBoi.x, (int) pathBoi.y + 1] != 1)
+              {
+                  isVaild.Add(new Vector2(pathBoi.x, pathBoi.y + 1));
+              }
+          }
+
+          if (pathBoi.x < 0)
+          {
+              if (grid[(int) pathBoi.x - 1, (int) pathBoi.y] != 1)
+              {
+                  isVaild.Add(new Vector2(pathBoi.x - 1, pathBoi.y));
+              }
+          }
+
+          if (pathBoi.y < 0)
+          {
+              if (grid[(int) pathBoi.x, (int) pathBoi.y - 1] != 1)
+              {
+                  isVaild.Add(new Vector2(pathBoi.x, pathBoi.y - 1));
+              }
+          }
+
+          if (isVaild.Count < 0)
+          {
+              for (int i = 0; i < isVaild.Count; i++)
+              {
+                  if (i == 0)
+                  {
+                      whereToMove = isVaild[0];
+                  }
+
+                  if (Vector2.Distance(isVaild[i], endPos < Vector2.Distance(whereToMove, endPos)))
+                  {
+                      whereToMove = isVaild[i];
+                  }
+              }
+              return whereToMove;
+          }
+
+          return lastSpot;
+      }
+
 
 }
